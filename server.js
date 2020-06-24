35416972fd7e84e1a9b4b44ac6d4c29b42a8d5e8
Kirill Yusov <kyusov@gmail.com>
2020-06-24 23:48:47 +0800

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3001
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({
     extended: false
